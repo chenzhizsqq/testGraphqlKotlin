cbb098ac05543d2f5bd58c03147f9aba67de1089
chenzhizsqq <chenzhi@qq.com>
2023-03-31 19:49:01 +0900

    设定 2

--- app/src/main/java/com/test/testgraphqlkotlin/MainActivity.kt
+++ app/src/main/java/com/test/testgraphqlkotlin/MainActivity.kt
@@ -2,10 +2,25 @@ package com.test.testgraphqlkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.apollographql.apollo3.ApolloClient
+import com.test.testgraphqlkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.test.setOnClickListener {
+            val apolloClient = ApolloClient.Builder()
+                .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql")
+                .build()
+
+            //val response = apolloClient.query(LaunchListQuery()).execute()
+            //Log.d("LaunchList", "Success ${response.data}")
+        }
+
     }
 }
